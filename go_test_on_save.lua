600bd956d1ec38c6da759b147be73847ea3c0f54
Searidang Pa <dangsyncpa@gmail.com>
2025-03-03 15:20:40 -0500

    init

---
+++ go_test_on_save.lua
@@ -0,0 +1,293 @@
+require 'config.util_find_func'
+require 'config.util_go_test_on_save'
+
+local mini_notify = require 'mini.notify'
+local make_notify = mini_notify.make_notify {}
+
+local ignored_actions = {
+  pause = true,
+  cont = true,
+  start = true,
+  skip = true,
+}
+
+---@class attachInstance
+---@field group number
+---@field ns number
+---@field job_id number
+local attach_instace = {
+  group = -1,
+  ns = -1,
+  job_id = -1,
+}
+
+---@class winState
+---@field floating table
+---@field buf number
+---@field win number
+local win_state = {
+  floating = {
+    buf = -1,
+    win = -1,
+  },
+}
+
+---@class entry
+---@field Package string
+---@field Test string
+---@field Action string
+---@field Output string
+
+local group_name = 'live_go_test_group'
+local ns_name = 'live_go_test_ns'
+
+---@param entry entry
+local make_key = function(entry)
+  assert(entry.Package, 'Must have package name' .. vim.inspect(entry))
+  if not entry.Test then
+    return entry.Package
+  end
+  assert(entry.Test, 'Must have test name' .. vim.inspect(entry))
+  return string.format('%s/%s', entry.Package, entry.Test)
+end
+
+---@class testState
+---@field bufnr number
+---@field tests table<string, table>
+---@field all_output table
+
+---@param bufnr number
+---@param test_state testState
+---@param entry entry
+local add_golang_test = function(bufnr, test_state, entry)
+  local testLine = Find_test_line_by_name(bufnr, entry.Test)
+  if not testLine then
+    testLine = 0
+  end
+  test_state.tests[make_key(entry)] = {
+    name = entry.Test,
+    line = testLine - 1,
+    output = {},
+  }
+end
+
+---@param test_state testState
+---@param entry entry
+local add_golang_output = function(test_state, entry)
+  assert(test_state.tests, vim.inspect(test_state))
+  local trimmed_output = vim.trim(entry.Output)
+  table.insert(test_state.tests[make_key(entry)].output, vim.trim(entry.Output))
+end
+
+---@param test_state testState
+---@param entry entry
+local mark_outcome = function(test_state, entry)
+  local test = test_state.tests[make_key(entry)]
+  if not test then
+    return
+  end
+  test.success = entry.Action == 'pass'
+end
+
+---@param test_state testState
+---@param bufnr number
+local on_exit_fn = function(test_state, bufnr)
+  attach_instace.job_id = -1
+  local failed = {}
+  for _, test in pairs(test_state.tests) do
+    if not test.line or test.success then
+      goto continue
+    end
+
+    table.insert(failed, {
+      bufnr = bufnr,
+      lnum = test.line,
+      col = 0,
+      severity = vim.diagnostic.severity.ERROR,
+      source = 'go-test',
+      message = 'Test Failed',
+      user_data = {},
+    })
+
+    ::continue::
+  end
+
+  if #failed == 0 then
+    make_notify 'Test passed'
+  else
+    make_notify 'Test failed'
+  end
+
+  vim.diagnostic.set(attach_instace.ns, bufnr, failed, {})
+end
+
+---@param bufnr number
+---@param command string
+local attach_to_buffer = function(bufnr, command)
+  local test_state = {
+    bufnr = bufnr,
+    tests = {},
+    all_output = {},
+  }
+
+  vim.api.nvim_create_user_command('OutputAllTest', function()
+    Go_test_all_output(test_state, win_state)
+  end, {})
+  vim.api.nvim_create_user_command('OutputOneTest', function()
+    Toggle_test_output(test_state, win_state)
+  end, {})
+  vim.keymap.set('n', '<leader>to', function()
+    Toggle_test_output(test_state, win_state)
+  end, { desc = 'Test [O]utput', buffer = bufnr })
+
+  local extmark_ids = {}
+  vim.api.nvim_create_autocmd('BufWritePost', {
+    group = attach_instace.group,
+    pattern = '*.go',
+    callback = function()
+      Clean_up_prev_job(attach_instace.job_id)
+
+      attach_instace.job_id = vim.fn.jobstart(command, {
+        shell = true,
+        stdout_buffered = true,
+        on_stdout = function(_, data)
+          if not data then
+            return
+          end
+
+          for _, line in ipairs(data) do
+            if line == '' then
+              goto continue
+            end
+            local decoded = vim.json.decode(line)
+            assert(decoded, 'Failed to decode: ' .. line)
+            table.insert(test_state.all_output, decoded)
+
+            if ignored_actions[decoded.Action] then
+              goto continue
+            end
+
+            if decoded.Action == 'run' then
+              add_golang_test(bufnr, test_state, decoded)
+              goto continue
+            end
+
+            if decoded.Action == 'output' then
+              if decoded.Test then
+                add_golang_output(test_state, decoded)
+              end
+              goto continue
+            end
+
+            local test = test_state.tests[make_key(decoded)]
+            if not test then
+              goto continue
+            end
+
+            if decoded.Action == 'pass' then
+              mark_outcome(test_state, decoded)
+
+              local test_extmark_id = extmark_ids[test.name]
+              if test_extmark_id then
+                vim.api.nvim_buf_del_extmark(bufnr, attach_instace.ns, test_extmark_id)
+              end
+
+              local current_time = os.date '%H:%M:%S'
+              extmark_ids[test.name] = vim.api.nvim_buf_set_extmark(bufnr, attach_instace.ns, test.line, -1, {
+                virt_text = {
+                  { string.format('%s %s', '✅', current_time) },
+                },
+              })
+            end
+
+            if decoded.Action == 'fail' then
+              mark_outcome(test_state, decoded)
+              local test_extmark_id = extmark_ids[test.name]
+              if test_extmark_id then
+                vim.api.nvim_buf_del_extmark(bufnr, attach_instace.ns, test_extmark_id)
+              end
+            end
+
+            ::continue::
+          end
+        end,
+
+        on_exit = function()
+          on_exit_fn(test_state, bufnr)
+        end,
+      })
+    end,
+  })
+end
+
+local clear_group_ns = function()
+  if attach_instace.group == nil or attach_instace.ns == nil then
+    return
+  end
+  local ok, _ = pcall(vim.api.nvim_get_autocmds, { group = group_name })
+  if not ok then
+    return
+  end
+  vim.api.nvim_del_augroup_by_name(group_name)
+  vim.api.nvim_buf_clear_namespace(vim.api.nvim_get_current_buf(), attach_instace.ns, 0, -1)
+  vim.diagnostic.reset()
+end
+
+local new_attach_instance = function()
+  attach_instace.group = vim.api.nvim_create_augroup(group_name, { clear = true })
+  attach_instace.ns = vim.api.nvim_create_namespace(ns_name)
+end
+
+local attach_all_go_test_in_buf = function()
+  clear_group_ns()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local testsInCurrBuf = Find_all_tests(bufnr)
+  local concatTestName = ''
+  for testName, _ in pairs(testsInCurrBuf) do
+    concatTestName = concatTestName .. testName .. '|'
+  end
+  concatTestName = concatTestName:sub(1, -2) -- remove the last |
+  local command = { 'go', 'test', './...', '-json', '-v', '-run', string.format('%s', concatTestName) }
+  new_attach_instance()
+  attach_to_buffer(bufnr, command)
+end
+
+local attach_go_test = function()
+  clear_group_ns()
+  local test_name = Get_enclosing_test()
+  make_notify(string.format('Attaching test: %s', test_name))
+  local command = { 'go', 'test', './...', '-json', '-v', '-run', test_name }
+  new_attach_instance()
+  attach_to_buffer(vim.api.nvim_get_current_buf(), command)
+end
+
+local function drive_test_command()
+  local test_name = Get_enclosing_test()
+  make_notify(string.format('Attaching drive test: %s', test_name))
+  local command = { 'go', 'test', './integration_tests/*.go', '-json', '-v', '-run', test_name }
+  local command_str = string.format('go test integration_tests/*.go -json -v -run %s', test_name)
+  return command_str
+end
+
+local function drive_test_on_save_dev()
+  vim.env.UKS = 'others'
+  vim.env.MODE = 'dev'
+  new_attach_instance()
+  attach_to_buffer(vim.api.nvim_get_current_buf(), drive_test_command())
+end
+
+local function drive_test_on_save_staging()
+  vim.env.UKS = 'others'
+  vim.env.MODE = 'staging'
+  new_attach_instance()
+  attach_to_buffer(vim.api.nvim_get_current_buf(), drive_test_command())
+end
+
+vim.api.nvim_create_user_command('GoTestClear', clear_group_ns, {})
+vim.api.nvim_create_user_command('DriveTestOnSaveDev', drive_test_on_save_dev, {})
+vim.api.nvim_create_user_command('DriveTestOnSaveStaging', drive_test_on_save_staging, {})
+vim.api.nvim_create_user_command('GoTestOnSave', attach_go_test, {})
+vim.api.nvim_create_user_command('GoTestOnSaveBuf', attach_all_go_test_in_buf, {})
+vim.keymap.set('n', '<leader>gt', attach_go_test, { desc = '[T]oggle [G]o Test on save' })
+
+return {}
---
+++ util_find_func.lua
@@ -0,0 +1,142 @@
+local get_node_text = vim.treesitter.get_node_text
+local ts_utils = require('nvim-treesitter.ts_utils')
+
+local test_function_query_string = [[
+(function_declaration
+  name: (identifier) @name
+  parameters: (parameter_list
+    (parameter_declaration
+      name: (identifier)
+      type: (pointer_type
+        (qualified_type
+          package: (package_identifier) @_package_name
+          name: (type_identifier) ))))
+  (#eq? @_package_name "testing")
+  (#eq? @name "%s")
+)
+]]
+
+Find_test_line_by_name = function(go_bufnr, testName)
+  local formatted = string.format(test_function_query_string, testName)
+  local query = vim.treesitter.query.parse('go', formatted)
+  local parser = vim.treesitter.get_parser(go_bufnr, 'go', {})
+  local tree = parser:parse()[1]
+  local root = tree:root()
+
+  for _, node in query:iter_captures(root, go_bufnr, 0, -1) do
+    local nodeContent = get_node_text(node, go_bufnr)
+
+    if nodeContent == testName then
+      local start_line, _, _, _ = node:range()
+      return start_line + 1
+    end
+  end
+end
+
+local function_query = [[
+(function_declaration
+  name: (identifier) @name
+  parameters: (parameter_list
+    (parameter_declaration
+      name: (identifier)
+      type: (pointer_type
+        (qualified_type
+          package: (package_identifier) @_package_name
+          name: (type_identifier) ))))
+)
+]]
+
+function Find_all_tests (go_bufnr)
+  local query = vim.treesitter.query.parse('go', function_query)
+  local parser = vim.treesitter.get_parser(go_bufnr, 'go', {})
+  local tree = parser:parse()[1]
+  local root = tree:root()
+  assert(root, 'root is nil')
+
+  local res = {}
+  for _, node in query:iter_captures(root, go_bufnr, 0, -1) do
+    if node == nil then
+      return res
+    end
+    local nodeContent = get_node_text(node, go_bufnr)
+
+    -- all tests start with Test_
+    if not string.match(nodeContent, 'Test_') then
+      goto continue
+    end
+
+    res[nodeContent] = node:start() + 1
+    ::continue::
+  end
+  return res
+end
+
+
+
+---@param bufnr number
+---@param line number
+---@return TSNode|nil
+local function nearest_function_at_line(bufnr, line)
+  local lang = vim.treesitter.language.get_lang(vim.bo[bufnr].filetype) -- Get language from filetype
+  local parser = vim.treesitter.get_parser(bufnr, lang)
+  local tree = parser:parse()[1]
+  local root = tree:root()
+  assert(tree, 'tree is nil')
+  assert(parser, 'parser is nil')
+  assert(root, 'root is nil')
+
+  local function traverse(node)
+    local nearest_function = nil
+    for child in node:iter_children() do
+      if child:type() == 'function_declaration' or child:type() == 'method_declaration' then
+        local start_row, _, end_row, _ = child:range()
+        if start_row <= line and end_row >= line then
+          nearest_function = child
+        end
+      end
+
+      if not nearest_function then
+        nearest_function = traverse(child)
+      end
+      if nearest_function then
+        break
+      end
+    end
+    return nearest_function
+  end
+
+  return traverse(root)
+end
+
+function Nearest_func_name()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+  local line = cursor_pos[1] - 1
+  local func_node = nearest_function_at_line(bufnr, line)
+  assert(func_node, 'No function found')
+  for child in func_node:iter_children() do
+      if child:type() == 'identifier' or child:type() == 'field_identifier' or child:type() == 'name' then
+          return vim.treesitter.get_node_text(child, bufnr)
+      end
+  end
+end
+
+vim.api.nvim_create_user_command('NearestFuncName', function()
+  local func_name = Nearest_func_name()
+  print("Nearest func name: " .. func_name)
+end, {})
+
+---@return TSNode
+function Nearest_func_node()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+  local line = cursor_pos[1] - 1
+  local func_node = nearest_function_at_line(bufnr, line)
+  assert(func_node, 'No function found')
+  return func_node
+end
+
+
+
+
+
