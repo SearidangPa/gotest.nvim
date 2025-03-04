6b6e579c2b639c60fc8876ba822bc36a0f3e4fde
Searidang Pa <dangsyncpa@gmail.com>
2025-03-03 20:22:46 -0500

    Man is tormented by no greater anxiety than to find an individual consciousness. -- Fyodor Dostoevsky

--- go_test_on_save.lua
+++ go_test_on_save.lua
@@ -1,4 +1,5 @@
 M = {}
+
 require("config.util_find_func")
 require("config.util_go_test_on_save")
 
@@ -51,7 +52,6 @@ end
 
 local add_golang_output = function(test_state, entry)
 	assert(test_state.tests, vim.inspect(test_state))
-	local trimmed_output = vim.trim(entry.Output)
 	table.insert(test_state.tests[make_key(entry)].output, vim.trim(entry.Output))
 end
 
@@ -93,8 +93,80 @@ local on_exit_fn = function(test_state, bufnr)
 	vim.diagnostic.set(attach_instace.ns, bufnr, failed, {})
 end
 
+M.start_test = function(command, test_state, bufnr, extmark_ids)
+	return vim.fn.jobstart(command, {
+		shell = true,
+		stdout_buffered = true,
+		on_stdout = function(_, data)
+			if not data then
+				return
+			end
+
+			for _, line in ipairs(data) do
+				if line == "" then
+					goto continue
+				end
+				local decoded = vim.json.decode(line)
+				assert(decoded, "Failed to decode: " .. line)
+				table.insert(test_state.all_output, decoded)
+
+				if ignored_actions[decoded.Action] then
+					goto continue
+				end
+
+				if decoded.Action == "run" then
+					add_golang_test(bufnr, test_state, decoded)
+					goto continue
+				end
+
+				if decoded.Action == "output" then
+					if decoded.Test then
+						add_golang_output(test_state, decoded)
+					end
+					goto continue
+				end
+
+				local test = test_state.tests[make_key(decoded)]
+				if not test then
+					goto continue
+				end
+
+				if decoded.Action == "pass" then
+					mark_outcome(test_state, decoded)
+
+					local test_extmark_id = extmark_ids[test.name]
+					if test_extmark_id then
+						vim.api.nvim_buf_del_extmark(bufnr, attach_instace.ns, test_extmark_id)
+					end
+
+					local current_time = os.date("%H:%M:%S")
+					extmark_ids[test.name] = vim.api.nvim_buf_set_extmark(bufnr, attach_instace.ns, test.line, -1, {
+						virt_text = {
+							{ string.format("%s %s", "✅", current_time) },
+						},
+					})
+				end
+
+				if decoded.Action == "fail" then
+					mark_outcome(test_state, decoded)
+					local test_extmark_id = extmark_ids[test.name]
+					if test_extmark_id then
+						vim.api.nvim_buf_del_extmark(bufnr, attach_instace.ns, test_extmark_id)
+					end
+				end
+
+				::continue::
+			end
+		end,
+
+		on_exit = function()
+			on_exit_fn(test_state, bufnr)
+		end,
+	})
+end
+
 ---@param command string
-M.attach_to_buffer = function(bufnr, command)
+M.start_new_test = function(bufnr, command)
 	local test_state = {
 		bufnr = bufnr,
 		tests = {},
@@ -112,84 +184,8 @@ M.attach_to_buffer = function(bufnr, command)
 	end, { desc = "Test [O]utput", buffer = bufnr })
 
 	local extmark_ids = {}
-	vim.api.nvim_create_autocmd("BufWritePost", {
-		group = attach_instace.group,
-		pattern = "*.go",
-		callback = function()
-			Clean_up_prev_job(attach_instace.job_id)
-
-			attach_instace.job_id = vim.fn.jobstart(command, {
-				shell = true,
-				stdout_buffered = true,
-				on_stdout = function(_, data)
-					if not data then
-						return
-					end
-
-					for _, line in ipairs(data) do
-						if line == "" then
-							goto continue
-						end
-						local decoded = vim.json.decode(line)
-						assert(decoded, "Failed to decode: " .. line)
-						table.insert(test_state.all_output, decoded)
-
-						if ignored_actions[decoded.Action] then
-							goto continue
-						end
-
-						if decoded.Action == "run" then
-							add_golang_test(bufnr, test_state, decoded)
-							goto continue
-						end
-
-						if decoded.Action == "output" then
-							if decoded.Test then
-								add_golang_output(test_state, decoded)
-							end
-							goto continue
-						end
-
-						local test = test_state.tests[make_key(decoded)]
-						if not test then
-							goto continue
-						end
-
-						if decoded.Action == "pass" then
-							mark_outcome(test_state, decoded)
-
-							local test_extmark_id = extmark_ids[test.name]
-							if test_extmark_id then
-								vim.api.nvim_buf_del_extmark(bufnr, attach_instace.ns, test_extmark_id)
-							end
-
-							local current_time = os.date("%H:%M:%S")
-							extmark_ids[test.name] =
-								vim.api.nvim_buf_set_extmark(bufnr, attach_instace.ns, test.line, -1, {
-									virt_text = {
-										{ string.format("%s %s", "✅", current_time) },
-									},
-								})
-						end
-
-						if decoded.Action == "fail" then
-							mark_outcome(test_state, decoded)
-							local test_extmark_id = extmark_ids[test.name]
-							if test_extmark_id then
-								vim.api.nvim_buf_del_extmark(bufnr, attach_instace.ns, test_extmark_id)
-							end
-						end
-
-						::continue::
-					end
-				end,
-
-				on_exit = function()
-					on_exit_fn(test_state, bufnr)
-				end,
-			})
-		end,
-	})
+	Clean_up_prev_job(attach_instace.job_id)
+	attach_instace.job_id = M.start_test(command, test_state, bufnr, extmark_ids)
 end
 
 local clear_group_ns = function()
@@ -221,7 +217,7 @@ local attach_all_go_test_in_buf = function()
 	concatTestName = concatTestName:sub(1, -2) -- remove the last |
 	local command_str = string.format("go test ./... -json -v -run %s", concatTestName)
 	new_attach_instance()
-	M.attach_to_buffer(bufnr, command_str)
+	M.start_new_test(bufnr, command_str)
 end
 
 local attach_go_test = function()
@@ -231,11 +227,12 @@ local attach_go_test = function()
 	local command_str = string.format("go test ./... -json -v -run %s", test_name)
 
 	new_attach_instance()
-	M.attach_to_buffer(vim.api.nvim_get_current_buf(), command_str)
+	M.start_new_test(vim.api.nvim_get_current_buf(), command_str)
 end
 
-vim.api.nvim_create_user_command("GoTestClear", clear_group_ns, {})
-vim.api.nvim_create_user_command("GoTestOnSave", attach_go_test, {})
-vim.api.nvim_create_user_command("GoTestOnSaveBuf", attach_all_go_test_in_buf, {})
+M.setup = function()
+	vim.api.nvim_create_user_command("GoTest", attach_go_test, {})
+	vim.api.nvim_create_user_command("GoTestBuf", attach_all_go_test_in_buf, {})
+end
 
 return M
